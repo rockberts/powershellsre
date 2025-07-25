d3b42c6ad5febdb9dfe2cca3b18d0c229ab5fd41
rockberts <rockberts@gmail.com>
2025-07-24 22:27:38 -0400

    Update and rename Get-GPT4OMiniUsage.ps1 to Get-Model-Quotare.ps1

--- Get-Model-Quotare.ps1
+++ Get-Model-Quotare.ps1
@@ -6,6 +6,9 @@ $subscriptionId = az account show --query id -o tsv
 # Región a consultar
 $region = 'eastus'
 
+# Modelo a consultar
+$model = 'gpt-4o-mini'
+
 # Obtener el uso de los servicios cognitivos en la región especificada
 $results = az cognitiveservices usage list `
     --subscription $subscriptionId `
@@ -13,5 +16,5 @@ $results = az cognitiveservices usage list `
 
 # Filtrar y mostrar el uso del modelo 'gpt-4o-mini'
 $results | ConvertFrom-Json | Where-Object {
-    $_.name.value -match 'gpt-4o-mini'
+    $_.name.value -match $model
 } | Select-Object @{Name="Model";Expression={($_.name.value -split '\.')[-1]}}, currentValue, limit
